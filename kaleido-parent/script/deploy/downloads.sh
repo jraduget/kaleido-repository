66a62e220d089938e26887e9198940982c77f7b6
jraduget <jraduget@gmail.com>
2012-01-01 15:17:55 +0100

    0.8.1 release

--- kaleido-parent/script/deploy/downloads.sh
+++ kaleido-parent/script/deploy/downloads.sh
@@ -1,6 +1,6 @@
 
 # check jar deploy on google download code
-curl http://kaleido-foundry.googlecode.com/files/kaleido-distrib-0.8.1-bin.zip > /dev/null
+curl http://kaleido-foundry.googlecode.com/files/kaleido-distrib-0.8.1-bundle.zip > /dev/null
 curl http://kaleido-foundry.googlecode.com/files/kaleido-distrib-0.8.1-SNAPSHOT-bin.zip > /dev/null
 
 
