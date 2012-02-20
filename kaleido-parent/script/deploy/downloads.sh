4f12543d893cd5be3f74973e748fb74c87303b85
jraduget <jraduget@gmail.com>
2012-02-20 22:16:47 +0100

    prepare release

--- kaleido-parent/script/deploy/downloads.sh
+++ kaleido-parent/script/deploy/downloads.sh
@@ -1,7 +1,14 @@
+if [ $# -eq 0 ]
+then
+  cpt=1
+else
+  cpt=$1
+fi
 
-# check jar deploy on google download code
-curl http://kaleido-foundry.googlecode.com/files/kaleido-distrib-0.8.1-bundle.zip > /dev/null
-curl http://kaleido-foundry.googlecode.com/files/kaleido-distrib-0.8.1-SNAPSHOT-bin.zip > /dev/null
-
-
+for ((i = 0; i <= cpt; i += 1))
+do
+  # check jar deploy on google download code
+  curl http://kaleido-foundry.googlecode.com/files/kaleido-distrib-0.8.1-bundle.zip > /dev/null
+  curl http://kaleido-foundry.googlecode.com/files/kaleido-distrib-0.8.1-SNAPSHOT-bin.zip > /dev/null
+done
 
