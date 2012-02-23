b2e156b9eebd2ccc07a4c576322e30156191bef8
jraduget <jraduget@gmail.com>
2012-02-23 15:33:07 +0100

    prepare maven release

--- kaleido-parent/script/deploy/release.sh
+++ kaleido-parent/script/deploy/release.sh
@@ -1,5 +1,11 @@
+export MAVEN_OPT=-Xmx1024m
+
 # update child version after updating parent version
-mvn -N versions:update-child-modules
+# mvn -N versions:update-child-modules
+
+# site and google code upload
+mvn clean site:site && mvn site:stage
+# mvn site:deploy - does not work with maven-site 3 and svn scm  
 
 # signing gpg 
 # https://docs.sonatype.org/display/Repository/How+To+Generate+PGP+Signatures+With+Maven
@@ -7,14 +13,11 @@ mvn -N versions:update-child-modules
 # releasing
 # https://docs.sonatype.org/display/Repository/Sonatype+OSS+Maven+Repository+Usage+Guide
 
-mvn clean deploy -Dgpg.keyname=thekeyname -Dgpg.passphrase=thephrase
+mvn clean deploy -Dgpg.keyname=${GPG_KEYNAME} -Dgpg.passphrase=${GPG_PASSPHRASE}
 mvn release:clean
 mvn release:prepare
-mvn release:rollback
-mvn release:perform -Dgpg.keyname=thekeyname -Darguments=-Dgpg.passphrase=thephrase
+#mvn release:rollback
+mvn release:perform -Dgpg.keyname=${GPG_KEYNAME} -Darguments=-Dgpg.passphrase=${GPG_PASSPHRASE}
 
 
-# site and google code upload
-mvn clean site:site && mvn site:stage
-mvn site:deploy 
 
