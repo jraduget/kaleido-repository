cb8afb193c5c1e1987f5da24498d118ac1374429
jraduget <jraduget@gmail.com>
2015-10-28 21:47:11 +0100

    #44 - kaleido-mailing module - try to fix a build hive issue with the default smtp port 25

--- kaleido-mailing/src/test/java/org/kaleidofoundry/mail/dispatcher/SynchronousMailDispatcherTest.java
+++ kaleido-mailing/src/test/java/org/kaleidofoundry/mail/dispatcher/SynchronousMailDispatcherTest.java
@@ -25,98 +25,101 @@ import org.kaleidofoundry.core.util.StringHelper;
 import org.kaleidofoundry.mail.MailException;
 
 import com.dumbster.smtp.MailMessage;
+import com.dumbster.smtp.ServerOptions;
 import com.dumbster.smtp.SmtpServer;
 import com.dumbster.smtp.SmtpServerFactory;
 
 @RunWith(KaleidoJunit4ClassRunner.class)
 @NamedConfigurations(value = { @NamedConfiguration(name = "local-dumbster-session", uri = "classpath:/mailing/localDumbsterSession.yaml"),
-	  @NamedConfiguration(name = "synchronous-dispatcher", uri = "classpath:/mailing/synchronousDispatcher.yaml") })
+	@NamedConfiguration(name = "synchronous-dispatcher", uri = "classpath:/mailing/synchronousDispatcher.yaml") })
 public class SynchronousMailDispatcherTest extends AbstractSynchronousMailDispatcherTest {
 
-    private SmtpServer server;
+   private SmtpServer server;
 
-    private MailDispatcher mailDispatcher;
+   private MailDispatcher mailDispatcher;
 
-    @Before
-    public void setup() {
-	  server = SmtpServerFactory.startServer();
-	  mailDispatcher = MailDispatcherFactory.provides("myDispatcher");
-    }
+   @Before
+   public void setup() {
+	ServerOptions options = new ServerOptions();
+	options.port = 1025;
+	server = SmtpServerFactory.startServer(options);
+	mailDispatcher = MailDispatcherFactory.provides("myDispatcher");
+   }
 
-    @After
-    public void cleanup() throws Exception {
-	  server.stop();
-	  mailDispatcher.close();
-    }
+   @After
+   public void cleanup() throws Exception {
+	server.stop();
+	mailDispatcher.close();
+   }
 
-    @Override
-    protected MailDispatcher getMailDispatcher() {
-	  return mailDispatcher;
-    }
+   @Override
+   protected MailDispatcher getMailDispatcher() {
+	return mailDispatcher;
+   }
 
-    @Override
-    public void sendMail() throws MailException {
-	  super.sendMail();
-	  mailAssertions(server, 0);
-    }
+   @Override
+   public void sendMail() throws MailException {
+	super.sendMail();
+	mailAssertions(server, 0);
+   }
 
-    @Override
-    public void sendMailWithAttachments() throws MailDispatcherException, MailException, FileNotFoundException, IOException {
-	  super.sendMailWithAttachments();
-	  mailWithAttachmentsAssertions(server, 0);
-    }
+   @Override
+   public void sendMailWithAttachments() throws MailDispatcherException, MailException, FileNotFoundException, IOException {
+	super.sendMailWithAttachments();
+	mailWithAttachmentsAssertions(server, 0);
+   }
 
-    public static void mailAssertions(SmtpServer server, int mailIndex) {
-	  assertEquals(1, server.getEmailCount());
-	  assertEquals(String.valueOf(MAIL_PRIORITY), server.getMessage(mailIndex).getFirstHeaderValue("X-Priority"));
-	  assertEquals(FROM_ADRESS, server.getMessage(mailIndex).getFirstHeaderValue("From"));
-	  assertArrayEquals(TO_ADRESS, extractEmails(server.getMessage(mailIndex), "To"));
-	  assertArrayEquals(CC_ADRESS, extractEmails(server.getMessage(mailIndex), "Cc"));
-	  // assertArrayEquals(BCC_ADRESS, extractEmails(server.getMessage(mailIndex), "Bcc"));
-	  assertEquals(MAIL_SUBJECT, server.getMessage(mailIndex).getFirstHeaderValue("Subject"));
-	  assertTrue(server.getMessage(mailIndex).getBody().contains(MAIL_BODY_HTML));
-	  assertTrue(server.getMessage(mailIndex).getBody().contains("Content-Type: " + (MAIL_HTML ? "text/html" : "text/plain")));
-	  assertTrue(server.getMessage(mailIndex).getBody().contains("charset=" + MAIL_ENCODING));
-    }
+   public static void mailAssertions(SmtpServer server, int mailIndex) {
+	assertEquals(1, server.getEmailCount());
+	assertEquals(String.valueOf(MAIL_PRIORITY), server.getMessage(mailIndex).getFirstHeaderValue("X-Priority"));
+	assertEquals(FROM_ADRESS, server.getMessage(mailIndex).getFirstHeaderValue("From"));
+	assertArrayEquals(TO_ADRESS, extractEmails(server.getMessage(mailIndex), "To"));
+	assertArrayEquals(CC_ADRESS, extractEmails(server.getMessage(mailIndex), "Cc"));
+	// assertArrayEquals(BCC_ADRESS, extractEmails(server.getMessage(mailIndex), "Bcc"));
+	assertEquals(MAIL_SUBJECT, server.getMessage(mailIndex).getFirstHeaderValue("Subject"));
+	assertTrue(server.getMessage(mailIndex).getBody().contains(MAIL_BODY_HTML));
+	assertTrue(server.getMessage(mailIndex).getBody().contains("Content-Type: " + (MAIL_HTML ? "text/html" : "text/plain")));
+	assertTrue(server.getMessage(mailIndex).getBody().contains("charset=" + MAIL_ENCODING));
+   }
 
-    public static void mailWithAttachmentsAssertions(SmtpServer server, int mailIndex) {
-	  assertEquals(1, server.getEmailCount());
-	  assertEquals(String.valueOf(MAIL_PRIORITY), server.getMessage(mailIndex).getFirstHeaderValue("X-Priority"));
-	  assertEquals(FROM_ADRESS, server.getMessage(mailIndex).getFirstHeaderValue("From"));
-	  assertArrayEquals(TO_ADRESS, extractEmails(server.getMessage(mailIndex), "To"));
-	  assertArrayEquals(CC_ADRESS, extractEmails(server.getMessage(mailIndex), "Cc"));
-	  // assertArrayEquals(BCC_ADRESS, extractEmails(server.getMessage(mailIndex), "Bcc"));
-	  assertEquals(MAIL_SUBJECT, server.getMessage(mailIndex).getFirstHeaderValue("Subject"));
-	  assertTrue(server.getMessage(mailIndex).getBody().contains(MAIL_BODY_HTML));
-	  assertTrue(server.getMessage(mailIndex).getBody().contains("Content-Type: " + (MAIL_HTML ? "text/html" : "text/plain")));
-	  assertTrue(server.getMessage(mailIndex).getBody().contains("charset=" + MAIL_ENCODING));
+   public static void mailWithAttachmentsAssertions(SmtpServer server, int mailIndex) {
+	assertEquals(1, server.getEmailCount());
+	assertEquals(String.valueOf(MAIL_PRIORITY), server.getMessage(mailIndex).getFirstHeaderValue("X-Priority"));
+	assertEquals(FROM_ADRESS, server.getMessage(mailIndex).getFirstHeaderValue("From"));
+	assertArrayEquals(TO_ADRESS, extractEmails(server.getMessage(mailIndex), "To"));
+	assertArrayEquals(CC_ADRESS, extractEmails(server.getMessage(mailIndex), "Cc"));
+	// assertArrayEquals(BCC_ADRESS, extractEmails(server.getMessage(mailIndex), "Bcc"));
+	assertEquals(MAIL_SUBJECT, server.getMessage(mailIndex).getFirstHeaderValue("Subject"));
+	assertTrue(server.getMessage(mailIndex).getBody().contains(MAIL_BODY_HTML));
+	assertTrue(server.getMessage(mailIndex).getBody().contains("Content-Type: " + (MAIL_HTML ? "text/html" : "text/plain")));
+	assertTrue(server.getMessage(mailIndex).getBody().contains("charset=" + MAIL_ENCODING));
 
-	  assertTrue(server.getMessage(mailIndex).getBody().contains("Content-Type: text/html; charset=UTF-8"));
-	  assertTrue(server.getMessage(mailIndex).getBody().contains("Content-Transfer-Encoding: 7bit"));
+	assertTrue(server.getMessage(mailIndex).getBody().contains("Content-Type: text/html; charset=UTF-8"));
+	assertTrue(server.getMessage(mailIndex).getBody().contains("Content-Transfer-Encoding: 7bit"));
 
-	  assertTrue(server.getMessage(mailIndex).getBody().contains("Content-Type: text/xml; charset=UTF-8; name=helloworld.xml"));
-	  assertTrue(server.getMessage(mailIndex).getBody().contains("Content-Transfer-Encoding: quoted-printable"));
-	  assertTrue(server.getMessage(mailIndex).getBody().contains("Content-Disposition: attachment; filename=helloworld.xml"));
-	  assertTrue(server.getMessage(mailIndex).getBody().contains("<line>this is an email attachment with some specials characters</line>"));
+	assertTrue(server.getMessage(mailIndex).getBody().contains("Content-Type: text/xml; charset=UTF-8; name=helloworld.xml"));
+	assertTrue(server.getMessage(mailIndex).getBody().contains("Content-Transfer-Encoding: quoted-printable"));
+	assertTrue(server.getMessage(mailIndex).getBody().contains("Content-Disposition: attachment; filename=helloworld.xml"));
+	assertTrue(server.getMessage(mailIndex).getBody().contains("<line>this is an email attachment with some specials characters</line>"));
 
-	  assertTrue(server.getMessage(mailIndex).getBody().contains("Content-Type: image/gif; name=helloworld.gif"));
-	  assertTrue(server.getMessage(mailIndex).getBody().contains("Content-Transfer-Encoding: base64"));
-	  assertTrue(server.getMessage(mailIndex).getBody().contains("Content-Disposition: attachment; filename=helloworld.gif"));
-	  assertTrue(server.getMessage(mailIndex).getBody().contains("R0lGODlhvAIsAfcAAHPLLiEkIqQnKhkoKjNKYjNUJ9Amc3FrJJAmarInV/LMmP4mavvmsycoHTYo"));
+	assertTrue(server.getMessage(mailIndex).getBody().contains("Content-Type: image/gif; name=helloworld.gif"));
+	assertTrue(server.getMessage(mailIndex).getBody().contains("Content-Transfer-Encoding: base64"));
+	assertTrue(server.getMessage(mailIndex).getBody().contains("Content-Disposition: attachment; filename=helloworld.gif"));
+	assertTrue(server.getMessage(mailIndex).getBody().contains("R0lGODlhvAIsAfcAAHPLLiEkIqQnKhkoKjNKYjNUJ9Amc3FrJJAmarInV/LMmP4mavvmsycoHTYo"));
 
-	  assertTrue(server.getMessage(mailIndex).getBody().contains("Content-Type: text/plain; charset=us-ascii; name=helloworld.txt"));
-	  assertTrue(server.getMessage(mailIndex).getBody().contains("Content-Transfer-Encoding: 7bit"));
-	  assertTrue(server.getMessage(mailIndex).getBody().contains("Content-Disposition: attachment; filename=helloworld.txt"));
-	  assertTrue(server.getMessage(mailIndex).getBody().contains("this is an email attachment"));
-    }
+	assertTrue(server.getMessage(mailIndex).getBody().contains("Content-Type: text/plain; charset=us-ascii; name=helloworld.txt"));
+	assertTrue(server.getMessage(mailIndex).getBody().contains("Content-Transfer-Encoding: 7bit"));
+	assertTrue(server.getMessage(mailIndex).getBody().contains("Content-Disposition: attachment; filename=helloworld.txt"));
+	assertTrue(server.getMessage(mailIndex).getBody().contains("this is an email attachment"));
+   }
 
-    private static String[] extractEmails(MailMessage message, String headerField) {
-	  String[] adresses = StringHelper.split(message.getFirstHeaderValue(headerField), ",");
-	  if (adresses != null) {
-		for (int i = 0; i < adresses.length; i++) {
-		    adresses[i] = adresses[i].trim();
-		}
-	  }
-	  return adresses;
-    }
+   private static String[] extractEmails(MailMessage message, String headerField) {
+	String[] adresses = StringHelper.split(message.getFirstHeaderValue(headerField), ",");
+	if (adresses != null) {
+	   for (int i = 0; i < adresses.length; i++) {
+		adresses[i] = adresses[i].trim();
+	   }
+	}
+	return adresses;
+   }
 }
