28ceee8372ac12643b8958ebed943afdfcc36342
jraduget <jraduget@gmail.com>
2021-11-18 22:07:20 +0100

    Fix & cleanup security issue before release

--- kaleido-parent/script/deploy/site.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# -> create a site directory in the tags/$version/     and import mvn site content to it
-
-# deprecated
-cp -rf ../../target/site/* ../../../kaleido-site/site/
-find ../../../kaleido-site/site/  -name '*.html' -exec dos2unix {} \;
